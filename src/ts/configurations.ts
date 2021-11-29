696deda07e42c3230e56fd4c9790c346900fc5c4
Shankar Raj <sgummaraju@ecovues.com>
2021-11-29 11:56:47 +0530

    Source code

---
+++ src/ts/configurations.ts
@@ -0,0 +1,32 @@
+let configs = { "dashboardservices": "https://wcidev.ecovues.com/dashboardwebservices/resources/", "authCredentials": "" };
+let password = "Welcome1";
+let username = "prachaveti";
+configs.authCredentials = btoa(username + ":" + password);
+
+export function loadConfigs() {
+    let configs = sessionStorage.getItem('configs')
+    console.log('Get Configurations from local storage before loading Configs API: ' + configs);
+    if (configs == null) {
+        console.log('Invoking Configs Async Function');
+        configsApi()
+            .then(data => {
+                console.log("Setting configurations to Local storage");
+                sessionStorage.setItem('configs', data);
+            });
+    }
+
+}
+
+async function configsApi() {
+    let fetchData = {
+        method: "GET",
+        headers: new Headers({
+            "Authorization": "Basic cHJhY2hhdmV0aTpXZWxjb21lMQ==",
+            RequestMode: "no-cors"
+        })
+    };
+    let response = await fetch( "https://wcidev.ecovues.com/dashboardwebservices/resources/dashboard/configs", fetchData);
+    let result = await response.json();
+    console.log('After configs API completed');
+    return result;
+}
